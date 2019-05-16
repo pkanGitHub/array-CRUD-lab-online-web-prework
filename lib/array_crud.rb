eae9af20e50c6e4f2d3d79dfd049dcdd48b5d2d3
Phyllis Kan <pyk9q7@gmail.com>
2019-05-16 21:02:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
-  
+  my_array = ["rainning", "sunny", "cloudy", "hailing"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array.push("lighting")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array.unshift("storming")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  my_array["am"]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  my_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_array[-1]
 end
